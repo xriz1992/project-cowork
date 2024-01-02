42312266c041a0154b94d5dff06d6fde23ded43f
Chris <xriz1992@users.noreply.github.com>
2024-01-02 19:09:51 +0000

    no comment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: "AIzaSyDKi8cGAcleByejYCK2e6EXdR_sZ7sjxXk",
     authDomain: "project-cowork.firebaseapp.com",
     projectId: "project-cowork",
-    databaseURL: "project-cowork.firebaseapp.com", // TODO ??? check this out in firebase console
+    databaseURL: "project-cowork.firebaseapp.com",
     storageBucket: "project-cowork.appspot.com",
     messagingSenderId: "618030447540",
     appId: "1:618030447540:web:d965f9ab11b6033e6010e1",
